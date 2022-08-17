7bae827a57668cd2e4dac78c06820e73491b1a35
Lucas Galdino <lucasgaldino7@gmail.com>
2022-08-17 17:49:57 -0300

    dia 6

--- Bullseye/Bullseye/Views/ContentView.swift
+++ Bullseye/Bullseye/Views/ContentView.swift
@@ -1,100 +1,111 @@
-  //
-  //  ContentView.swift
-  //  Bullseye
-  //
-  //  Created by Lucas on 09/08/22.
-  //
+    //
+    //  ContentView.swift
+    //  Bullseye
+    //
+    //  Created by Lucas on 09/08/22.
+    //
 
 import SwiftUI
 
 struct ContentView: View {
-  @State private var alertIsVisible = false
-  @State private var sliderValue = 50.0
-  @State private var game = Game()
-  
-  var body: some View {
-    ZStack {
-      BackgroundView(game: $game                          )
-      VStack {
-        InstructionsView(game: $game)
-        SliderView(sliderValue: $sliderValue)
-        HitMeButton(alertIsVisible: $alertIsVisible, sliderValue: $sliderValue, game: $game)
-      }
+    @State private var alertIsVisible = false
+    @State private var sliderValue = 50.0
+    @State private var game = Game()
+    
+    var body: some View {
+        ZStack {
+            BackgroundView(game: $game                          )
+            VStack {
+                InstructionsView(game: $game)
+                    .padding(.bottom, 100)
+                HitMeButton(alertIsVisible: $alertIsVisible, sliderValue: $sliderValue, game: $game)
+            }
+            SliderView(sliderValue: $sliderValue)
+        }
     }
-  }
 }
 
 struct InstructionsView: View {
-  @Binding var game: Game
-  
-  var body: some View {
-    VStack {
-      InstructionText(text: "🎯🎯🎯\nPut the Bullseye as close as you can to")
-        .padding(.leading, 30.0)
-        .padding(.trailing, 30.0)
-      BigNumberText(text: String(game.target))
+    @Binding var game: Game
+    
+    var body: some View {
+        VStack {
+            InstructionText(text: "🎯🎯🎯\nPut the Bullseye as close as you can to")
+                .padding(.leading, 30.0)
+                .padding(.trailing, 30.0)
+            BigNumberText(text: String(game.target))
+        }
     }
-  }
 }
 
 struct SliderView: View {
-  @Binding var sliderValue: Double
-  
-  var body: some View {
-    HStack {
-      SliderLabelText(text: "1")
-      Slider(value: $sliderValue, in: 1.0 ... 100.0)
-      SliderLabelText(text: "100")
+    @Binding var sliderValue: Double
+    
+    var body: some View {
+        HStack {
+            SliderLabelText(text: "1")
+            Slider(value: $sliderValue, in: 1.0 ... 100.0)
+            SliderLabelText(text: "100")
+        }
+        .padding()
     }
-    .padding()
-  }
 }
 
 struct HitMeButton: View {
-  @Binding var alertIsVisible: Bool
-  @Binding var sliderValue: Double
-  @Binding var game: Game
-  
-  var body: some View {
-    Button(action: {
-      print("Hello, SwiftUI!")
-      alertIsVisible = true
-    }) {
-      Text("Hit me".uppercased())
-        .bold()
-        .font(.title3)
+    @Binding var alertIsVisible: Bool
+    @Binding var sliderValue: Double
+    @Binding var game: Game
+    
+    var body: some View {
+        Button(action: {
+            alertIsVisible = true
+        }) {
+            Text("Hit me".uppercased())
+                .bold()
+                .font(.title3)
+        }
+        .padding(20.0)
+        .background(
+            ZStack {
+                Color("ButtonColor")
+                LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+            }
+        )
+        .foregroundColor(Color.white)
+        .cornerRadius(21.0)
+        .overlay(
+            RoundedRectangle(cornerRadius: 21.0)
+                .strokeBorder(Color.white, lineWidth: 2.0)
+        )
+        .alert(
+            "Hello there!",
+            isPresented: $alertIsVisible,
+            presenting: {
+                let roundedValue = Int(sliderValue.rounded())
+                return (
+                    roundedValue,
+                    game.points(sliderValue: roundedValue)
+                )
+            } () as (roundedValue: Int, points: Int)
+        ) { data in
+            Button("Awesome!") {
+                game.startNewRound(points: data.points)
+            }
+        } message: { data in
+            Text("The slider's value is \(data.roundedValue).\n" + "You scored \(data.points) points this round.")
+        }
     }
-    .padding(20.0)
-    .background(
-      ZStack {
-        Color("ButtonColor")
-        LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
-      }
-    )
-    .foregroundColor(Color.white)
-    .cornerRadius(21.0)
-    .overlay(
-      RoundedRectangle(cornerRadius: 21.0)
-        .strokeBorder(Color.white, lineWidth: 2.0)
-    )
-    .alert("Hello there!", isPresented: $alertIsVisible) {
-      Button("Awesome!") {}
-    } message: {
-      let roundedValue = Int(sliderValue.rounded())
-      Text("The slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round.")
-    }
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-    ContentView()
-      .previewInterfaceOrientation(.landscapeRight)
-    ContentView()
-      .preferredColorScheme(.dark)
-    ContentView()
-      .preferredColorScheme(.dark)
-      .previewInterfaceOrientation(.landscapeRight)
-  }
+    static var previews: some View {
+        ContentView()
+        ContentView()
+            .previewInterfaceOrientation(.landscapeRight)
+        ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.dark)
+            .previewInterfaceOrientation(.landscapeRight)
+    }
 }
--- Bullseye/BullseyeTests/BullseyeTests.swift
+++ Bullseye/BullseyeTests/BullseyeTests.swift
@@ -1,9 +1,9 @@
-//
-//  BullseyeTests.swift
-//  BullseyeTests
-//
-//  Created by Lucas on 09/08/22.
-//
+    //
+    //  BullseyeTests.swift
+    //  BullseyeTests
+    //
+    //  Created by Lucas on 09/08/22.
+    //
 
 import XCTest
 @testable import Bullseye
@@ -11,11 +11,11 @@ import XCTest
 class BullseyeTests: XCTestCase {
     
     var game: Game!
-
+    
     override func setUpWithError() throws {
         game = Game()
     }
-
+    
     override func tearDownWithError() throws {
         game = nil
     }
@@ -31,5 +31,31 @@ class BullseyeTests: XCTestCase {
         let score = game.points(sliderValue: guess)
         XCTAssertEqual(score, 95)
     }
-
+    
+    func testNewRound() {
+        game.startNewRound(points: 100)
+        XCTAssertEqual(game.score, 100)
+        XCTAssertEqual(game.round, 2)
+    }
+    
+    func testScoreExact() {
+        let guess = game.target
+        let score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 200)
+    }
+    
+    func testScoreClose() {
+        let guess = game.target + 2
+        let score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 98 + 50)
+    }
+    
+    func testRestart() {
+        game.startNewRound(points: 100)
+        XCTAssertNotEqual(game.score, 0)
+        XCTAssertNotEqual(game.round, 1)
+        game.restart()
+        XCTAssertEqual(game.score, 0)
+        XCTAssertEqual(game.round, 1)
+    }
 }
